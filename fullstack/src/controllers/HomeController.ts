b32529eb2b5ea17f4570a3f270b7fd538d546e80
mariana <mcarrasquilla323@gmail.com>
2026-08-03 19:47:01 -0500

    feat: implement book catalog functionality with MVC structure and EJS views

--- fullstack/src/controllers/HomeController.ts
+++ fullstack/src/controllers/HomeController.ts
@@ -1,4 +1,6 @@
-import type { Request, Response } from 'express'; 
+import type { Request, Response } from 'express';
+import { books } from '../data/Books.js';
+import { Book } from '../models/Book.js';  
 
 export class HomeController { 
   static index(req: Request, res: Response): void { 
@@ -13,5 +15,18 @@ export class HomeController {
     viewData["title"] = "About"; 
 
     res.render('home/about', { viewData: viewData }); 
-  } 
+  }
+
+  static Main_Point(req: Request, res: any) { 
+    const viewData: any = {}; 
+    viewData["books"] = books; 
+    res.render('home/books', viewData);
+  }
+
+  static show(req: any, res: any)  
+  { 
+    const book = Book.findById(books, parseInt(req.params.id)); 
+
+    res.render('home/show', { book: book }); 
+  }
 } 
